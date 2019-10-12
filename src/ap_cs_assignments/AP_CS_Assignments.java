53ed4214aa55db183c6217cad551a7841cdde5c2
tgirotti21 <tgirotti21@timberlane.net>
2019-10-12 01:12:40 -0400

    Initial Commit (Testing)
    If this works then the repo will be updated properly

---
+++ src/ap_cs_assignments/AP_CS_Assignments.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ap_cs_assignments;
+
+/**
+ *
+ * @author Thomas
+ */
+public class AP_CS_Assignments {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) 
+	{
+		Ch_1_Exercises.Spikey();
+		Ch_1_Exercises.Egg();
+	}
+}
---
+++ src/ap_cs_assignments/Ch_1_Exercises.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ap_cs_assignments;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Ch_1_Exercises {
+	public static void Spikey()
+	{
+		System.out.println("  \\/");
+		System.out.println(" \\\\//");
+		System.out.println("\\\\\\///");
+		System.out.println("///\\\\\\");
+		System.out.println(" //\\\\");
+		System.out.println("  /\\");
+	}
+	
+	public static void Egg()
+	{ // This output doesn't look right, especially compared to the book
+		System.out.println("   __________");
+		System.out.println("  /          \\");
+		System.out.println(" /            \\");
+		System.out.println(" _\" _\"_\'_\"_\'_\"_");
+		System.out.println(" \\            /");
+		System.out.println("  \\          /");
+		System.out.println("   __________");
+	}
+}
